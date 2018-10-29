9ba62a6332864ec050b1d487896aaf58002b0611
Mathijs Haakman <MathijsHaakman@outlook.com>
2018-10-29 23:43:02 +0100

    Formatted Arm class

--- src/Arm.h
+++ src/Arm.h
@@ -1,10 +1,9 @@
 //
-// Created by Keut on 5-9-2018.
+// Created by Mathijs Haakman on 5-9-2018.
 //
 
 #ifndef ROBOTARMMOTIONANDVISION_ARM_H
 #define ROBOTARMMOTIONANDVISION_ARM_H
-
 
 #include "Segment.h"
 #include "Vector.h"
@@ -16,20 +15,13 @@ private:
     double threshold = 0;
 
 public:
-    void setDestinationPoint(int x, int y);
-
     void addSegment(Segment segment);
-
-    void run();
-
-    void print(bool printAllSegments);
-
-    void printRealEndpoint();
-
-    void setThreshold(double distance);
-
     bool isNearDestinationPoint();
+    void print(bool printAllSegments);
+    void printRealEndpoint();
+    void run();
+    void setDestinationPoint(int x, int y);
+    void setThreshold(double distance);
 };
-
 
 #endif //ROBOTARMMOTIONANDVISION_ARM_H
